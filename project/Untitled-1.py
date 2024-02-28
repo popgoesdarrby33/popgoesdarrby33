0e57325cfad56082d53d0e198446a2390650039c
popgoesdarrby33 <davisbiko@gmail.com>
2024-02-28 19:20:30 +0300

    second commit

---
+++ project/Untitled-1.py
@@ -0,0 +1,5 @@
+name= input("what is your name")
+age = input("What is your age? ")
+location = input("Where do you live? ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
